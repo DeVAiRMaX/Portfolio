bf8ecfce8094f5376f1a606f42e904e5c2a1518c
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2025-02-11 22:30:20 +0100

    #10

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -13,18 +13,18 @@ export class FeedbackComponent {
 
   feedbackState = {
     comment1: {
-      text: "Anna hat das Team mit ihrer großartigen Vision und Führungsstärke motiviert. Ohne ihre klare Richtung wären wir nicht so erfolgreich gewesen.",
-      author: "J. Müller - Projektleiter",
+      text: "Max hat das Team mit seinen großartigen Vision motiviert. Ohne seine klare Richtung wären wir nicht so erfolgreich gewesen.",
+      author: "J. Müller - Teamkollegin",
       image: './assets/img/usercomment1.png' 
     },
     comment2: {
-      text: "Lukas hat das Team hervorragend durch schwierige Zeiten geführt. Seine unermüdliche Energie und Kommunikation waren der Schlüssel zu unserem Erfolg.",
-      author: "M. Meyer - Senior Entwickler",
+      text: "Max hat das Team hervorragend durch schwierige Zeiten geführt. Seine unermüdliche Energie und Kommunikation waren der Schlüssel zu unserem Erfolg.",
+      author: "M. Meyer - Teamkollege",
       image: './assets/img/usercomment2.png'  
     },
     comment3: {
-      text: "Sophie hat das Team mit ihrer außergewöhnlichen Problemlösungsfähigkeit und ihrem Engagement zur Höchstleistung angespornt. Wir haben viel erreicht, dank ihrer Unterstützung.",
-      author: "A. Schmidt - Marketing Expertin",
+      text: "Max hat das Team mit seiner außergewöhnlichen Problemlösungsfähigkeit und ihrem Engagement zur Höchstleistung angespornt. Wir haben viel erreicht, dank seiner Unterstützung.",
+      author: "A. Schmidt - Teamkollege",
       image: './assets/img/usercomment3.png'
     },
   }
